83c09b082bd4583cec28d3f2af1bbdf55a102a1d
aigul <halitovaajgul210@gmail.com>
2025-05-22 15:59:01 +0500

    all arrays are returned

--- cpp_part/pybind/source.cpp
+++ cpp_part/pybind/source.cpp
@@ -1,7 +1,7 @@
 #include <vector>
-#include <algorithm>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <cstring>
 
 namespace py = pybind11;
 
@@ -22,23 +22,28 @@ py::tuple perform_calculation(
     size_t n = v_buf.size;
 
     std::vector<double> v_filtered1, z_filtered1;
-    std::vector<double> z_steps, v_steps;
+    std::vector<double> v_steps, z_steps;
 
-    for (int i = 0; i < n; i++) {
+    for (size_t i = 0; i < n; i++) {
         double z = z_ptr[i];
         double v = v_ptr[i];
-        if (v != undef_val && z >= min_z && z <= max_z) {
-            v_filtered1.push_back(v);
-            z_filtered1.push_back(z);
+        if (v != undef_val && z >= 3 && z <= 7) {
+            v_filtered1.push_back(-v);
+            z_filtered1.push_back(-z);
             v_steps.push_back(v);
             z_steps.push_back(z);
         }
     }
-    
-    py::array_t<double> v_result(v_steps.size(), v_steps.data());
-    py::array_t<double> z_result(v_steps.size(), v_steps.data());
-    py::array_t<double> v_steps_result(v_steps.size(), v_steps.data());
-    py::array_t<double> z_steps_result(z_steps.size(), z_steps.data());
+
+    py::array_t<double> v_result(v_filtered1.size());
+    py::array_t<double> z_result(z_filtered1.size());
+    py::array_t<double> v_steps_result(v_steps.size());
+    py::array_t<double> z_steps_result(z_steps.size());
+
+    std::memcpy(v_result.mutable_data(), v_filtered1.data(), v_filtered1.size() * sizeof(double));
+    std::memcpy(z_result.mutable_data(), z_filtered1.data(), z_filtered1.size() * sizeof(double));
+    std::memcpy(v_steps_result.mutable_data(), v_steps.data(), v_steps.size() * sizeof(double));
+    std::memcpy(z_steps_result.mutable_data(), z_steps.data(), z_steps.size() * sizeof(double));
 
     return py::make_tuple(
         z_result,
@@ -48,9 +53,8 @@ py::tuple perform_calculation(
     );
 }
 
-
 PYBIND11_MODULE(example, m) {
-    m.def("perform_calculation_pybind", &perform_calculation,
+    m.def("perform_calculation", &perform_calculation,
         py::arg("z_array"),
         py::arg("v_array"),
         py::arg("min_z"),
